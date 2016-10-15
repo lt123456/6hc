49da701a8f734deb9e34cb92447eb303f3200c72
lt123456 <1416040754@qq.com>
2016-10-15 18:03:47 +0800

    add

--- Application/Admin/Controller/LotteryRecordController.class.php
+++ Application/Admin/Controller/LotteryRecordController.class.php
@@ -23,19 +23,19 @@ class LotteryRecordController extends BaseController {
     public function index()
     {
          $map =array_filter(I());
-
         // 分页
-        $count = $this ->lotteryModel->where($this->serach())->count();
+        $count = $this ->lotteryModel->where($this->serach($map))->count();
         $Page  = new \Think\Page($count,2);// 实例化分页类 传入总记录数和每页显示的记录数(25)
-        if($this->serach()){
+        if($this->serach($map)){
 
-            foreach($this->serach() as $key=>$val) {
+            foreach($this->serach($map) as $key=>$val) {
                 $Page->parameter[$key]   =   urlencode($val);
             }
         }
+        var_dump($this->serach($map));
         $show  = $Page->show();
         $periodsRes =  $this ->lotteryModel
-                                ->where($this->serach)
+                                ->where($this->serach($map))
                                 ->join('__ADMIN__ ON __LOTTERY_RECORD__.admin_id = __ADMIN__.id')
                                 ->limit($Page->firstRow.','.$Page->listRows)
                                 ->field('6hc_admin.id,6hc_admin.username,6hc_lottery_record.*');
@@ -52,22 +52,23 @@ class LotteryRecordController extends BaseController {
         $this->assign('periods',$periods);
         $this->display();
     }
-    public function  serach()
+    public function  serach($map)
     {
+
         if(isset($map['periods'])){
-           $where['periods'] = array('gt',$map['periods']);
+           $where['periods'] = array('eq',$map['periods']);
         }
         if(isset($map['year'])){
-            $where['lottery_time'] = array('like','%'.$map['periods'].'%');
+            $where['lottery_time'] = array('like','%'.$map['year'].'%');
         }
         if(isset($map['week'])){
-            $where['week'] = array('gt',$map['week']);
+            $where['week'] = array('eq',$map['week']);
         }
         if(isset($map['periods'])){
-            $where['periods'] = array('gt',$map['periods']);
+            $where['periods'] = array('eq',$map['periods']);
         }
         if(isset($map['singleDouble'])){
-            $where['special_one_two'] = array('gt',$map['singleDouble']);
+            $where['special_one_two'] = array('eq',$map['singleDouble']);
         }
         return $where;
     }
