be18196e0273aab9f3e4b0ae9cce213ebcc7a733
lt123456 <1416040754@qq.com>
2016-11-09 00:41:32 +0800

    index init

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -2,8 +2,22 @@
 namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
-    public function index(){
-      	
+
+
+	protected  $lotteryRecord;
+	public function __construct()
+	{
+		parent::__construct();
+		$this->lotteryRecord = D('lottery_record');
+
+	}
+
+    public function index(){  
+
+    	$lotteryRecords  = $this->lotteryRecord->limit(5)->select();
+
+        $this->assign('lotteryRecord',$lotteryRecords);
+        $this->display();
         $this->display();
 
     }
