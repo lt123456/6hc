78278e6751a97ed3487fd78f1266167527277eb9
lt123456 <1416040754@qq.com>
2016-11-08 15:07:16 +0800

    frontent init

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -5,12 +5,6 @@ class IndexController extends Controller {
     public function index(){
       	
         echo '电脑';die;
-      	if($id ==1){
-
-      		$this->theme('mobile')->display();
-      	}else{
-      		$this->theme('pc')->display();
-      	}
 
     }
 }
--- Application/Mobile/Controller/IndexController.class.php
+++ Application/Mobile/Controller/IndexController.class.php
@@ -5,12 +5,6 @@ class IndexController extends Controller {
     public function index(){
       	
         echo '手机';die;
-      	if($id ==1){
-
-      		$this->theme('mobile')->display();
-      	}else{
-      		$this->theme('pc')->display();
-      	}
 
     }
 }
