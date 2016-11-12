de10e43b532a041333dc7d64a634dad69d2c02b1
liutao <1416040754@qq.com>
2016-11-12 22:40:24 +0800

    Mark

--- Application/Home/Controller/MarkController.class.php
+++ Application/Home/Controller/MarkController.class.php
@@ -18,30 +18,42 @@ class MarkController extends BaseController
         $data = I();
         $history_year =  $this->lotteryRecord->group("year")->order('year desc')->field('year')->select();
 
-        $where =  !isset($data['year']) ? date('Y') : $data['year'];
+        $where =  empty($data['year']) ? date('Y') : $data['year'];
         $type  =  !isset($data['type']) ? 1 : $data['type'];
-        $history_records  = $this->lotteryRecord->where(array('year'=>$where,'status'=>'active'))
-                                 ->field('id,periods,just_string,animal_string,five_string,special_five,special_animal,special_number,special_one_two,created_at')
-                                ->order('id desc')->select();
+        $history_records = S(md5($where));
+        if(empty($record)){
+            $history_records  = $this->lotteryRecord->where(array('year'=>$where,'status'=>'active'))
+                ->field('id,periods,just_string,animal_string,five_string,special_five,special_animal,special_number,special_one_two,created_at')
+                ->order('id desc')->select();
 
-        foreach($history_records as $key=>$val) {
-
-            $records[$key]['periods']  = $history_records[$key]['periods'];
-            $records[$key]['created_at']  = $history_records[$key]['created_at'];
-            $records[$key]['just_string']  = $history_records[$key]['just_string'];
-            $records[$key]['just_type'] = $this->nutureJustType($history_records[$key],$type);
-            $records[$key]['special_number']  = $history_records[$key]['special_number'];
-            $records[$key]['special_type']  = $this->nutureSpecialType($history_records[$key],$type);
-            $records[$key]['one_two']  =  ( $history_records[$key]['special_number'] % 2) ? '单' : '双';
-            $records[$key]['big_small']  =  ( $history_records[$key]['special_number'] > 24) ? '大' : '小';
-            $records[$key]['sum']  = $this->sum($history_records[$key]['just_string'], $history_records[$key]['special_number']);
-            $records[$key]['sum_big_small']  =  ( $history_records[$key]['sum'] > 174) ? '大' : '小';
-
+            S($where,$history_records,C('CACHE_TIME')['base']);
         }
 
-        var_dump($records);
+        $url = md5($_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"]);
+        $records  =S($url);
+        if(empty($records)) {
+            foreach($history_records as $key=>$val) {
+
+                $records[$key]['periods']  = $history_records[$key]['periods'];
+                $records[$key]['created_at']  = $history_records[$key]['created_at'];
+                $records[$key]['just_string']  = $history_records[$key]['just_string'];
+                $records[$key]['just_type'] = $this->nutureJustType($history_records[$key],$type);
+                $records[$key]['special_number']  = $history_records[$key]['special_number'];
+                $records[$key]['special_type']  = $this->nutureSpecialType($history_records[$key],$type);
+                $records[$key]['one_two']  =  ( $history_records[$key]['special_number'] % 2) ? '单' : '双';
+                $records[$key]['big_small']  =  ( $history_records[$key]['special_number'] > 24) ? '大' : '小';
+                $records[$key]['sum']  = $this->sum($history_records[$key]['just_string'], $history_records[$key]['special_number']);
+                $records[$key]['sum_one_two']  =  ( $records[$key]['sum'] %2 ) ? '单' : '双';
+                $records[$key]['sum_big_small']  =  ( $records[$key]['sum'] > 174) ? '大' : '小';
+
+            }
+            S($url,$records,C('CACHE_TIME')['base']);
+        }
+
+
         $this->lotteryRecord->getLastSql();
         $this->assign('history_year',$history_year);
+        $this->assign('records',$records);
         $this->display();
 
     }
@@ -62,6 +74,12 @@ class MarkController extends BaseController
         return $sum;
     }
 
+    /***
+     *
+     * @param $res
+     * @param $type
+     * @return string
+     */
     public function  nutureJustType($res,$type) {
 
             $result = $res['animal_string'];
@@ -70,7 +88,6 @@ class MarkController extends BaseController
                 case '1':
                     $result = $res['animal_string'];
                     break;
-
                 case '2':
                     $result = $res['five_string'];
                     break;
